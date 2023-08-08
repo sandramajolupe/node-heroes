03501e7794849f18cf522acc97466f046eba2510
huxley <elhuxley277@gmail.com>
2023-08-08 14:18:42 -0500

    feature -> model -> hero model

--- models/hero.js
+++ models/hero.js
@@ -1,19 +1,26 @@
 const mongoose = require('mongoose')
 
 const Schema = mongoose.Schema
-const AuthorSchema = new Schema ({
+const HeroSchema = new Schema ({
     first_name: {
         type:String,
         require: true,
         maxLength:100
     },
-    family_name:{
+
+    faction:{
         type:String,
         require: true,
         maxLength:100
     },
+
+    hp:{
+        type:Number,
+        require: true,
+        default:10
+    }
     // date_of_birth: { type: Date },
     // date_of_death: { type: Date },
 })
 
-module.exports = mongoose.model('Author', AuthorSchema)
+module.exports = mongoose.model('Hero', AuthorSchema)
